ee3b4e9c1fc5d0a03969775dbb9510e41944a9f4
Alexandre Insua <alexandreinsua@gmail.com>
2021-10-10 19:47:10 +0200

    Building dictionary added
    Also I fixed a bug into randomColorGen.js file because there was a mistaken in the endind of the first line.

--- JavaScript/randomColorGen.js
+++ JavaScript/randomColorGen.js
@@ -1,2 +1,2 @@
-let a = () = > "#" + Math.random().toString(16).substring(2, 8).to 
-console.log("Random color is " +  a() )
+let a = () => "#" + Math.random().toString(16).substring(2, 8)
+console.log("Random color is " + a())
